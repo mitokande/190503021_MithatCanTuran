3515678c212e5478ca10962a36be6351594fb6d8
Mithat Can Turan <e190503021@stud.tau.edu.tr>
2022-04-07 22:31:32 +0300

    Initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,12 @@
+module com.example._190503021_mithat_can {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+
+    opens com.example._190503021_mithat_can to javafx.fxml;
+    exports com.example._190503021_mithat_can;
+}
